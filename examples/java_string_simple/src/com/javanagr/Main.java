2126af9c5c2864fefa754cf3c384b2e961f7bc8e
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2018-04-22 19:56:18 -0700

    Add java_string_simple example
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

---
+++ examples/java_string_simple/src/com/javanagr/Main.java
@@ -0,0 +1,16 @@
+package com.javanagr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String input_1 = "Hello";
+        String input_2 = "Good";
+        if (input_1.equals(input_2)) {
+            System.out.println("Win");
+        }
+        else {
+            System.out.println("Fail");
+        }
+        System.out.println("DONE");
+    }
+}
