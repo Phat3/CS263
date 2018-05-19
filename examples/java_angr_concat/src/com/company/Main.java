76979af46dbfaf972df81f776f48bbfa831fa1c7
Sydney Ma <siyi.ma0523@gmail.com>
2018-05-18 17:11:06 -0700

    StringConcat main

---
+++ examples/java_angr_concat/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String str1 = "Sydney";
+	    String str2 = "IceCream";
+	    String res = str1.concat(str2);
+
+	    String str3 = args[0];
+	    String str4 = args[1];
+	    String symRes = str3.concat(str4);
+    }
+}
